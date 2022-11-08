d8745057422b741f81ff7e03aa1a230114773066
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2022-11-08 14:32:57 +0530

    added updated user contract

--- routes/contract.js
+++ routes/contract.js
@@ -4,19 +4,18 @@ const router = express.Router();
 const Web3 = require('web3');
 
 const web3 = new Web3(process.env.wsUrl);
+const contract = require('../bin/User.json');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    let contractAbi = require('../bin/User.abi.json')
       res.json({
-          'data': contractAbi
+          'data': contract.abi
       });
 });
 
 router.post('/', async (req, res)=>{
-    let contractAbi = require('../bin/User.abi.json');
     console.log(req.body);
-    let userContract = new web3.eth.Contract(contractAbi);
+    let userContract = new web3.eth.Contract(contract.abi);
 
     const fundingAccount = web3.eth.accounts.privateKeyToAccount(process.env.fundingAccount);
 
@@ -25,18 +24,18 @@ router.post('/', async (req, res)=>{
     await web3.eth.sendTransaction({
         from: fundingAccount.address,
         to: account.address,
-        value: 1000000000000000000
+        value: 10000000000000000000 // 10**19 equivalent to 10 ether
     });
 
     const transaction = userContract.deploy({
-        data: process.env.contractByteCode,
+        data: contract.bytecode,
         arguments: [req.body.name, req.body.email, req.body.dob, req.body.country, req.body.mobile, req.body.gender]
     });
 
     const options = {
         data: transaction.encodeABI(),
-        gas: 1500000,
-        gasPrice: '300000000000'
+        gas: 3000000,
+        gasPrice: '3000000000000'
     };
 
     const signed  = await web3.eth.accounts.signTransaction(options, account.privateKey);
