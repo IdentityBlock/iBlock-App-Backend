3924cbef989b890d391df2295664447eb9009ab7
Akila-I <indurangakga.19@uom.lk>
2022-11-14 22:52:59 +0530

    sending ether to newly created account is set to use signedTransaction

--- routes/contract.js
+++ routes/contract.js
@@ -1,54 +1,85 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Web3 = require('web3');
+const Web3 = require("web3");
 
 const web3 = new Web3(process.env.wsUrl);
-const contract = require('../bin/User.json');
+const contract = require("../bin/User.json");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-      res.json({
-          'data': contract.abi
-      });
+router.get("/", function (req, res, next) {
+  res.json({
+    data: contract.abi,
+  });
 });
 
-router.post('/', async (req, res)=>{
-    console.log(req.body);
-    let userContract = new web3.eth.Contract(contract.abi);
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  let userContract = new web3.eth.Contract(contract.abi);
 
-    const fundingAccount = web3.eth.accounts.privateKeyToAccount(process.env.fundingAccount);
+  const fundingAccount = web3.eth.accounts.privateKeyToAccount(
+    process.env.fundingAccount
+  );
 
-    const account = web3.eth.accounts.create();
+  const account = web3.eth.accounts.create();
 
-    await web3.eth.sendTransaction({
-        from: fundingAccount.address,
-        to: account.address,
-        value: 10000000000000000000 // 10**19 equivalent to 10 ether
-    });
+  console.log(account.privateKey);
+  console.log(account.address);
+  console.log(fundingAccount.address);
 
-    const transaction = userContract.deploy({
-        data: contract.bytecode,
-        arguments: [req.body.name, req.body.email, req.body.dob, req.body.country, req.body.mobile, req.body.gender]
-    });
+  // await web3.eth.sendTransaction({
+  //   from: fundingAccount.address,
+  //   to: account.address,
+  //   value: 10000000000000000000, // 10**19 equivalent to 10 ether
+  // });
 
-    const options = {
-        data: transaction.encodeABI(),
-        gas: 3000000,
-        gasPrice: '3000000000000'
-    };
+  const initialTxSign = await web3.eth.accounts.signTransaction(
+    {
+      to: account.address,
+      value: "500000000000000000", // equivalent to 0.5 ether
+      gas: 2000000,
+    },
+    process.env.fundingAccount
+  );
+  console.log(initialTxSign);
+  const initialRecipt = await web3.eth.sendSignedTransaction(
+    initialTxSign.rawTransaction
+  );
 
-    const signed  = await web3.eth.accounts.signTransaction(options, account.privateKey);
-    const receipt = await web3.eth.sendSignedTransaction(signed.rawTransaction);
+  console.log(initialRecipt);
 
-    return res.status(200).json(
-        {
-            "private-key": account.privateKey,
-            "contract-address": receipt.contractAddress
-        }
-    );
+  const transaction = await userContract.deploy({
+    data: contract.bytecode,
+    arguments: [
+      req.body.name,
+      req.body.email,
+      req.body.dob,
+      req.body.country,
+      req.body.mobile,
+      req.body.gender,
+    ],
+  });
 
+  const options = {
+    data: transaction.encodeABI(),
+    gas: 3000000,
+  };
 
+  const signed = await web3.eth.accounts.signTransaction(
+    options,
+    account.privateKey
+  );
+
+  console.log(signed);
+
+  const receipt = await web3.eth.sendSignedTransaction(signed.rawTransaction);
+
+  console.log(receipt.contractAddress);
+
+  return res.status(200).json({
+    "private-key": account.privateKey,
+    "contract-address": receipt.contractAddress,
+  });
 });
 
 module.exports = router;
