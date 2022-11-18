b0e2be6c28d44315833b9471147bf6ea70c0aca2
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2022-11-18 10:10:07 +0530

    route to get logo

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   return res.json({
     'response': 'ok'
   });
+});
+
+router.get('/logo.png', function(req, res){
+  return res.sendFile(path.join(__dirname, '../mail_templates/images/iblock.png'));
 });
 
 module.exports = router;
